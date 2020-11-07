295111efcadec4dc48b541e492f17ba9acd9de16
davidgo0098 <david.gomez.vasquez.1998gmail.com>
2020-11-06 22:52:49 -0600

    archivos subidos

---
+++ miagenda/app/src/main/java/com/example/miagenda/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.miagenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.miagenda.clases.ConexionSQLite;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ConexionSQLite objConexion;
+    final String NOMBRE_BASE_DATOS = "miagenda";
+    Button botonAgregar;
+    ListView listaContactos;
+    ArrayList<String> lista;
+    ArrayAdapter adaptador;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        objConexion = new ConexionSQLite(MainActivity.this,NOMBRE_BASE_DATOS,null,1);
+
+            botonAgregar = findViewById(R.id.btnAgregar);
+
+            botonAgregar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent ventana = new Intent(MainActivity.this, RegistrarContacto.class);
+                    startActivity(ventana);
+                }
+            });
+
+            listaContactos = findViewById(R.id.lvContactos);
+            lista = llenarLista();
+            adaptador = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,lista);
+            listaContactos.setAdapter(adaptador);
+
+
+        }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            lista.clear();
+            lista = llenarLista();
+            adaptador = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1);
+            listaContactos.setAdapter(adaptador);
+        }catch (Exception error){
+            Toast.makeText(MainActivity.this, "Error:" + error.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public ArrayList llenarLista(){
+            ArrayList<String> miLista = new ArrayList<>();
+            SQLiteDatabase base = objConexion.getReadableDatabase();
+            String consulta = "select id_contacto, nombre, telefono from contactos order by nombre ASC";
+            Cursor cadaRegistro = base.rawQuery(consulta,null);
+            if (cadaRegistro.moveToFirst()){
+                do{
+                    miLista.add(cadaRegistro.getString(1).toString());
+                }while(cadaRegistro.moveToNext());
+            }
+            return miLista;
+        }
+
+    }
+
---
+++ miagenda/app/src/main/java/com/example/miagenda/RegistrarContacto.java
@@ -0,0 +1,76 @@
+package com.example.miagenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.miagenda.clases.ConexionSQLite;
+
+public class RegistrarContacto extends AppCompatActivity {
+
+    ConexionSQLite objConexion;
+    final String NOMBRE_BASE_DATOS = "miagenda";
+    EditText nombre, telefono;
+    Button botonAgregar, botonRegresar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_contacto);
+
+        objConexion = new ConexionSQLite(RegistrarContacto.this,NOMBRE_BASE_DATOS,null,1);
+
+        nombre  = findViewById(R.id.txtNombreCompleto);
+        telefono = findViewById(R.id.txtTelefono);
+        botonAgregar = findViewById(R.id.btnGuardarContacto);
+        botonRegresar = findViewById(R.id.btnRegresar);
+
+        //Asignar evento click a cada uno de los botones
+        botonAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Agregar
+                //Llamamos al metodo registrar
+                registrar();
+            }
+        });
+
+        botonRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Regresar
+                //Llamamos al metodo regresar
+                regresar();
+
+            }
+        });
+
+    }
+
+    //Creacion de metodos
+
+    private void regresar(){
+        Intent actividad = new Intent(RegistrarContacto.this,MainActivity.class);
+        startActivity(actividad);
+        RegistrarContacto.this.finish();
+
+    }
+
+    private void registrar(){
+        try {
+            SQLiteDatabase miBaseDatos = objConexion.getWritableDatabase();
+            String comando = "INSERT INTO contactos (nombre,telefono) VALUES ('"+ nombre.getText() +"','"+ telefono.getText() +"')";
+            miBaseDatos.execSQL(comando);
+            miBaseDatos.close();
+            Toast.makeText(RegistrarContacto.this,"Datos Registrado con exito", Toast.LENGTH_LONG).show();
+        }catch (Exception error){
+            Toast.makeText(RegistrarContacto.this,"Error" + error.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+}
